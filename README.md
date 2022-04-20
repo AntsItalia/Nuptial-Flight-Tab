8ce2b01f3e25fc670a03c9f3ab3f4e1e098d81cc
Luigi Francesco Ricatti <ricattilf@gmail.com>
2022-04-20 12:35:44 +0200

    First Commit
    Added "js" folder and sciamature.js
    Added "styles" folder and sciamature.css
    Added index.html

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Nuptial-Flight-Tab
-A table of the nuptial swarming flights of the main Italian ant species
+# Ants Italia
+
+## Nuptial Flight Tab
+
+This repository is une of my projects for the [YouTube channel Ants Italia](https://www.youtube.com/antsitalia) and for it's [domain](https://www.antsitalia.com/), services and it's [community of user](https://www.facebook.com/groups/antsitalia).
+
+
+<img src="./imgs/pc_ant.jpg" height="206" width="245" style="border-radius:50%">
+
---
+++ js/sciamature.js
@@ -0,0 +1,17 @@
+var calcHeight = function() 
+{
+    //var headerDimensions = 0; //$('#header-bar').height();
+    $('#preview-frame').height($(window).height());
+}
+     
+$(document).ready(function()
+{
+    calcHeight();
+    /*$('#header-bar a.close').mouseover(function() {
+      $('#header-bar a.close').addClass('activated');
+      }).mouseout(function() {
+      $('#header-bar a.close').removeClass('activated');
+      });*/
+});
+
+$(window).resize(function() { calcHeight(); } ).load(function() { calcHeight(); });
